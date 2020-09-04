846747141b43e318b1d4344aa7578aac7f7423d3
vishal <96449vishal@gmail.com>
2020-09-04 09:45:58 -0700

    make it better

---
+++ app.js
@@ -0,0 +1,36 @@
+const bodyParser = require('body-parser')
+const express = require('express')
+const app= express();
+const path=require('path')
+const PORT = process.env.PORT || 3000 
+app.use(bodyParser.urlencoded({
+    extended:true
+}))
+app.use(express.json())
+
+//app.use(morgan('dev'))
+//app.use(cors())
+app.set("view engine","ejs")
+app.use(express.static(path.join(__dirname,"assets")))
+
+app.get("/",(req,res)=>{
+    res.render('home')
+})
+app.get("/menu",(req,res)=>{
+res.render('menu')
+})
+app.get("/starter",(req,res)=>{
+res.render('starter')
+})
+app.get("/drinks",(req,res)=>{
+res.render('drinks')
+})
+app.get("/contactus",(req,res)=>{
+res.render('contactus')
+})
+app.get("/gallery",(req,res)=>{
+    res.render('gallery')
+    })
+app.listen(PORT,()=>{
+    console.log(PORT)
+})
